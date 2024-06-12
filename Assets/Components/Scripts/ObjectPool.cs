8c3ac8ed3d39f8de311bd12edbb4685bece5c5e7
Jonan <jjsalomonzon@gmail.com>
2024-06-12 16:26:24 -0700

    updated cook max objects

--- Assets/Components/Scripts/ObjectPool.cs
+++ Assets/Components/Scripts/ObjectPool.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public abstract class ObjectPool<T> : MonoBehaviour where T : MonoBehaviour
 {
     [SerializeField] private T prefab;
     [SerializeField] private float frequency;
+    [SerializeField] private int maxObjects;
     [SerializeField] protected PositionAnchor spawnPosition;
 
     private List<T> pool = new List<T>();
@@ -15,16 +17,24 @@ public abstract class ObjectPool<T> : MonoBehaviour where T : MonoBehaviour
     {
         for (var i = 0; i < 10; i++)
         {
-            var obj = Instantiate(prefab);
+            var obj = Instantiate(prefab, transform);
             obj.gameObject.SetActive(false);
             OnInstantiate(obj);
             pool.Add(obj);
         }
     }
 
+    private void OnDisable()
+    {
+        foreach(var obj in pool)
+        {
+            obj.gameObject.SetActive(false);
+        }
+    }
+
     private void Update()
     {
-        if (timer > frequency)
+        if (timer > frequency && pool.Where(obj => obj.gameObject.activeSelf).Count() < maxObjects)
         {
             var obj = pool[index % pool.Count];
             obj.transform.position = spawnPosition.Position;
