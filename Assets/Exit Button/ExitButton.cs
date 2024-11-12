1c2e2b50ae27a057ee2884f999d54a5174f2a4e3
Jonan <johnnan.monzon@borderlessvr.com>
2024-11-12 11:59:26 -0700

    added dj volume proximity

---
+++ Assets/Exit Button/ExitButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ExitButton : MonoBehaviour
+{
+    //private Button button;
+
+    //public event UnityAction OnClick;
+
+    //private void Awake()
+    //{
+    //    button.onClick.AddListener(() =>
+    //    {
+    //        OnClick?.Invoke();
+    //    });
+    //}
+}
