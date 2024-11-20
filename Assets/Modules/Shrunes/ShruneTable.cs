b93707d1072a6e0ced449fd0e7b1ee72e00e3f3b
Jonan <jjsalomonzon@gmail.com>
2024-11-19 22:30:10 -0800

    added major mushroom item

--- Assets/Modules/Shrunes/ShruneTable.cs
+++ Assets/Modules/Shrunes/ShruneTable.cs
@@ -1,15 +1,25 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ShruneTable : MonoBehaviour
 {
     [SerializeField] private GameManager gameManager;
+
+    //todo: create image button component maybe like Inventory slot
     [SerializeField] private GameObject inventoryButton;
     [SerializeField] private Image inventoryPreview;
 
+    [SerializeField] private GameObject mushroomButton;
+    [SerializeField] private Image mushroomPreview;
+
     private Item targetItem;
-    private GameObject item;
+    private GameObject spawnedItem;
     private Camera mainCamera;
+
+    private ItemInstance MushroomItem => gameManager.Inventory.Find(item => item.Data.name == "Mushroom");
+    private List<ItemInstance> MajorItems => gameManager.Inventory.FindAll(item => item != MushroomItem).ToList();
 
     private void Awake()
     {
@@ -20,32 +30,53 @@ public class ShruneTable : MonoBehaviour
     {
         if (gameManager.Inventory.Count > 0)
         {
-            targetItem = gameManager.Inventory[0].Data;
-            inventoryPreview.enabled = true;
-            inventoryPreview.sprite = targetItem.Sprite;
+            if (MushroomItem)
+            {
+                mushroomPreview.enabled = true;
+                mushroomPreview.sprite = MushroomItem.Data.Sprite;
+            }
+
+            if (MajorItems.Count > 0)
+            {
+                targetItem = MajorItems[0].Data;
+                inventoryPreview.enabled = true;
+                inventoryPreview.sprite = targetItem.Sprite;
+            }
         }
     }
 
-    public void SpawnItem()
+    public void SpawnMushroom()
     {
-        item = Instantiate(targetItem.ItemPrefab);
+        if (MushroomItem) SpawnItem(MushroomItem);
+    }
+
+    public void SpawnMajorItem()
+    {
+        if (MajorItems.Count > 0) SpawnItem(MajorItems[0]);
+    }
+
+    private void SpawnItem(ItemInstance item)
+    {
+        spawnedItem = Instantiate(item.Data.ItemPrefab);
         inventoryButton.SetActive(false);
+        mushroomButton.SetActive(false);
     }
 
     public void DropItem()
     {
-        item = null;
+        spawnedItem = null;
         inventoryButton.SetActive(true);
+        mushroomButton.SetActive(true);
     }
 
     private void Update()
     {
-        if (item)
+        if (spawnedItem)
         {
             var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit, 10f))
             {
-                item.transform.position = hit.point;
+                spawnedItem.transform.position = hit.point;
             }
 
             if (Input.GetMouseButtonUp(0))
