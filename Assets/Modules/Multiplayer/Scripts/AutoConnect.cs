150e243c42ce422159b9653289d2a3b16f59f2fe
Jonan <jjsalomonzon@gmail.com>
2024-12-02 21:41:42 -0800

    added bog room camp

---
+++ Assets/Modules/Multiplayer/Scripts/AutoConnect.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoConnect : MonoBehaviour
+{
+    [SerializeField] private MultiplayerArena arena;
+    [SerializeField] private MultiplayerManager multiplayerManager;
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private Transform spawnAnchor1;
+    //[SerializeField] private Button exitButton;
+
+    private List<string> firstNames = new List<string> { "Hollow", "Sharon", "Jesus", "Charmander", "Feni" };
+    private List<string> lastNames = new List<string> { "Zozo", "Venga", "Lotus", "Atari", "Bagus" };
+
+    private void Awake()
+    {
+        arena.Initialize(spawnAnchor1.position, spawnAnchor1.position);
+    }
+
+    private void Start()
+    {
+        var username = GenerateRandomName();
+        multiplayerManager.SignIn(username, () => StartCoroutine(AutoJoinBogRoom()));
+
+        PufferballPlayer.OnLocalPlayerSpawned += player =>
+        {
+            inputManager.SetControllable(player);
+            inputManager.OnInteractionButtonClicked += () => player.LaunchBall();
+        };
+    }
+
+    public string GenerateRandomName()
+    {
+        string firstName = firstNames[Random.Range(0, firstNames.Count)];
+        string lastName = lastNames[Random.Range(0, lastNames.Count)];
+        return $"{firstName} {lastName}";
+    }
+
+    private IEnumerator AutoJoinBogRoom()
+    {
+        yield return new WaitForSeconds(2f);
+
+        multiplayerManager.ListLobbies(async lobbies =>
+        {
+            if (lobbies.Count > 0)
+            {
+                await multiplayerManager.JoinLobbyById(lobbies[0].Id);
+            }
+            else
+            {
+                await multiplayerManager.CreateRelayAndLobby();
+            }
+        });
+    }
+}
