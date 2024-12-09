76db14e4727abc85d8613898101c8d6284d6cedc
Jonan <jjsalomonzon@gmail.com>
2024-12-08 21:47:45 -0800

    adjusted overhead interaction position

--- Assets/UI/Interactions/Scripts/OverheadInteractionIndicator.cs
+++ Assets/UI/Interactions/Scripts/OverheadInteractionIndicator.cs
@@ -6,12 +6,10 @@ public class OverheadInteractionIndicator : MonoBehaviour
     [SerializeField] private GameObject indicator;
     [SerializeField] private Button indicatorButton;
 
-    private Camera mainCamera;
     private ProximityAction proximityAction;
 
     private void Awake()
     {
-        mainCamera = Camera.main;
         proximityAction = GetComponentInParent<ProximityAction>();
         indicatorButton.onClick.AddListener(proximityAction.Use);
     }
@@ -19,8 +17,5 @@ public class OverheadInteractionIndicator : MonoBehaviour
     private void Update()
     {
         indicator.SetActive(proximityAction.Interactable && proximityAction.InRange);
-
-        var position = mainCamera.WorldToScreenPoint(proximityAction.transform.position);
-        transform.position = position;
     }
 }
