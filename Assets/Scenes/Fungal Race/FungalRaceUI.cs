9bdb0454e682535e75a4073311d563ab6e63195c
Jonan <jjsalomonzon@gmail.com>
2024-12-16 14:10:17 -0800

    update pitch

--- Assets/Scenes/Fungal Race/FungalRaceUI.cs
+++ Assets/Scenes/Fungal Race/FungalRaceUI.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class FungalRaceUI : MonoBehaviour
 {
     [SerializeField] private Controller controller;
+    [SerializeField] private AudioSource audioSource;
 
     private void Awake()
     {
@@ -13,10 +14,11 @@ public class FungalRaceUI : MonoBehaviour
         pitchSlider.onValueChanged.AddListener(value =>
         {
             // Map the value using a cubic curve
-            float curvedValue = Mathf.Pow(value / pitchSlider.maxValue, 1.5f); // Normalize by 3 and then cube
+            float curvedValue = Mathf.Pow(value / pitchSlider.maxValue, 1.5f);
             float speed = Mathf.Lerp(0, 5f, curvedValue);
 
             controller.Movement.SetSpeed(speed);
+            audioSource.pitch = value;
         });
     }
 }
