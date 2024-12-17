517969f9ee3bd80046c23552bf1e60963420c777
Jonan <jjsalomonzon@gmail.com>
2024-12-16 19:20:05 -0800

    add burst button

--- Assets/Scenes/Fungal Race/FungalRaceUI.cs
+++ Assets/Scenes/Fungal Race/FungalRaceUI.cs
@@ -9,57 +9,82 @@ public class FungalRaceUI : MonoBehaviour
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private Slider pitchSlider;
     [SerializeField] private Slider chargeSlider;
-
+    [SerializeField] private Button burstButton;
+    [SerializeField] private CanvasGroup canvasGroup;
 
     [SerializeField] private float baseMovementSpeed = 3f; // Maximum movement speed at 100% pitch
     [SerializeField] private float sprintMultiplier = 1.5f; // Maximum movement speed at 100% pitch
+    [SerializeField] private float burstSpeedMultiplier = 2f;
+    [SerializeField] private float burstEnergyDepletion = 1.5f;
+    [SerializeField] private float burstEnergyPrice = 0.5f;
     [SerializeField] private float walkMultiplier = 0.5f; // Maximum movement speed at 100% pitch
     [SerializeField] private float lowPitchChargeSpeed = 4f;    // Intensity of charge curve parabola
     [SerializeField] private float baseChargeSpeed = 2f;
     [SerializeField] private float highPitchEnergyRate = 4f;    // Intensity of charge curve parabola
     [SerializeField] private float maxPitch = 2f;
     [SerializeField] private float minPitch = 0.1f;
-
+    
     [SerializeField] private float charge;
     [SerializeField] private float chargeRate;
+    [SerializeField] private bool isBursting = false;
 
+    private float burstStartCharge;
     private void Awake()
     {
-
+        burstButton.onClick.AddListener(() =>
+        {
+            burstStartCharge = charge;
+            isBursting = true;
+        });
     }
 
     private void Update()
     {
-        chargeSlider.value = charge;
+        canvasGroup.interactable = !isBursting;
+        canvasGroup.alpha = isBursting ? 0.75f : 1f;
+        burstButton.interactable = !isBursting && charge >= burstEnergyPrice;
 
-        // Normalize the slider value to the range [0, 1]
         float movementSpeed;
-        float normalizedValue = pitchSlider.value / pitchSlider.maxValue;
-        if (normalizedValue < 0.33f)
+
+        if (isBursting)
         {
-            movementSpeed = baseMovementSpeed * walkMultiplier;
-            charge += lowPitchChargeSpeed * Time.deltaTime;
-            audioSource.pitch = minPitch;
-        }
-        else if (normalizedValue < 0.66f)
-        {
-            movementSpeed = baseMovementSpeed;
-            charge += baseChargeSpeed * Time.deltaTime;
-            audioSource.pitch = 1f;
+            movementSpeed = baseMovementSpeed * burstSpeedMultiplier;
+            charge -= burstEnergyDepletion * Time.deltaTime;
+            audioSource.pitch = maxPitch;
+            controller.Movement.SetSpeed(movementSpeed);
+
+
+            if (burstStartCharge - charge > burstEnergyPrice) isBursting = false;
         }
         else
         {
-            movementSpeed = baseMovementSpeed * sprintMultiplier;
-            charge -= highPitchEnergyRate * Time.deltaTime;
-            audioSource.pitch = maxPitch;
+            // Normalize the slider value to the range [0, 1]
+            float normalizedValue = pitchSlider.value / pitchSlider.maxValue;
+            if (normalizedValue < 0.33f)
+            {
+                movementSpeed = baseMovementSpeed * walkMultiplier;
+                charge += lowPitchChargeSpeed * Time.deltaTime;
+                audioSource.pitch = minPitch;
+            }
+            else if (normalizedValue < 0.66f)
+            {
+                charge += baseChargeSpeed * Time.deltaTime;
+                movementSpeed = baseMovementSpeed * ((charge * 0.25f) + 0.75f);
+                audioSource.pitch = 1f;
+            }
+            else
+            {
+                charge -= highPitchEnergyRate * Time.deltaTime;
+                audioSource.pitch = maxPitch;
+
+                movementSpeed = baseMovementSpeed * sprintMultiplier * ((charge * 0.75f) + 0.25f);
+
+            }
         }
 
+        controller.Movement.SetSpeed(movementSpeed);
+
         charge = Mathf.Clamp(charge, 0f, 1f);
-
-
-        controller.Movement.SetSpeed(movementSpeed * charge);
-
-
-
+        chargeSlider.value = charge;
     }
 }
