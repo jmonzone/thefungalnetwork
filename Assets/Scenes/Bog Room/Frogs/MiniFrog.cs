5f10e7a4d3b0d256c6053f5279c0f642f818c4a3
Jonan <jjsalomonzon@gmail.com>
2024-12-23 14:23:44 -0800

    updated walls

--- Assets/Scenes/Bog Room/Frogs/MiniFrog.cs
+++ Assets/Scenes/Bog Room/Frogs/MiniFrog.cs
@@ -4,36 +4,4 @@ using UnityEngine;
 
 public class MiniFrog : MonoBehaviour
 {
-    private Transform spore;
-    private Transform targetPosition;
-    private MovementController movement;
-    private MountController mountController;
-
-    private void Awake()
-    {
-        movement = GetComponent<MovementController>();
-        mountController = GetComponent<MountController>();
-        mountController.OnMounted += MountController_OnMounted;
-    }
-
-    private void MountController_OnMounted()
-    {
-        movement.Stop();
-        mountController.Mountable.Movement.SetPosition(targetPosition.position);
-    }
-
-    private void Update()
-    {
-        if (mountController && mountController.HasMount.Value && targetPosition && Vector3.Distance(transform.position, targetPosition.position) < 2f)
-        {
-            movement.Jump();
-        }
-    }
-
-    public void AssignSpore(Transform spore, Transform targetPosition)
-    {
-        this.spore = spore;
-        this.targetPosition = targetPosition;
-        movement.SetTarget(spore);
-    }
 }
