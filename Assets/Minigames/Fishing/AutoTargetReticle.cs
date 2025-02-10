29f4297d3beaf20d8e59e280d9f4af61faa51b6d
Jonan <jjsalomonzon@gmail.com>
2025-02-10 12:19:21 -0800

    cast fishing net

--- Assets/Minigames/Fishing/AutoTargetReticle.cs
+++ Assets/Minigames/Fishing/AutoTargetReticle.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class AutoTargetReticle : MonoBehaviour
 {
@@ -9,22 +10,24 @@ public class AutoTargetReticle : MonoBehaviour
     [SerializeField] private Transform reticle;
     [SerializeField] private float searchRadius = 3f;
 
+    public FishController TargetFishController { get; private set; } 
+
     private void Update()
     {
         if (!playerReference.Transform) return;
 
         // Find all FishController objects within the search radius
         Collider[] colliders = Physics.OverlapSphere(playerReference.Transform.position, searchRadius);
-        FishController closestFish = colliders
+        TargetFishController = colliders
             .Select(c => c.GetComponentInParent<FishController>())
             .Where(fish => fish != null)
             .OrderBy(fish => Vector3.Distance(playerReference.Transform.position, fish.transform.position))
             .FirstOrDefault();
 
-        if (closestFish)
+        if (TargetFishController)
         {
             reticle.gameObject.SetActive(true);
-            reticle.position = closestFish.transform.position;
+            reticle.position = TargetFishController.transform.position;
         }
         else
         {
---
+++ Assets/Minigames/Fishing/CastFishingNetButton.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CastFishingNetButton : MonoBehaviour
+{
+    [SerializeField] private AutoTargetReticle autoTargetReticle;
+    [SerializeField] private Button castFishingNetButton;
+    [SerializeField] private Transform fishingNet;
+    [SerializeField] private PlayerReference playerReference;
+    [SerializeField] private float duration = 1f;
+
+    private void Update()
+    {
+        castFishingNetButton.interactable = autoTargetReticle.TargetFishController;
+        castFishingNetButton.onClick.AddListener(() => StartCoroutine(ThrowNet()));
+    }
+
+    private IEnumerator ThrowNet()
+    {
+        var targetFish = autoTargetReticle.TargetFishController;
+        if (!targetFish) yield break;
+
+        fishingNet.gameObject.SetActive(true);
+
+        var start = playerReference.Transform.position + (targetFish.transform.position - playerReference.Transform.position).normalized * 0.5f;
+        var end = targetFish.transform.position;
+
+
+        fishingNet.position = start;
+
+        var xzDirection = end - start;
+        xzDirection.y = 0;
+        xzDirection.Normalize();
+
+        float elapsed = 0f;
+        float maxHeight = 2f; // Adjust this for the peak height of the toss
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / duration; // Progress from 0 to 1
+
+            // Interpolate position in XZ plane
+            var currentXZPosition = Vector3.Lerp(start, end, t);
+
+            // Calculate Y based on parabolic equation
+            float y = Mathf.Lerp(start.y, end.y, t) + maxHeight * (1 - 4 * (t - 0.5f) * (t - 0.5f)); // Parabola
+
+            // Update position
+            fishingNet.position = new Vector3(currentXZPosition.x, y, currentXZPosition.z);
+            fishingNet.Rotate(Vector3.down, Time.deltaTime * 10f);
+
+            yield return null;
+        }
+
+        // Ensure the net reaches the exact target position
+        fishingNet.transform.position = end;
+        targetFish.gameObject.SetActive(false);
+        yield return new WaitForSeconds(0.25f);
+
+        fishingNet.gameObject.SetActive(false);
+    }
+}
