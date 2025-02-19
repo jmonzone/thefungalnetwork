3325b802139311eaf0fb072cd9a0f779820add94
Jonan <jjsalomonzon@gmail.com>
2025-02-18 21:11:29 -0800

    added pufferball minigame

--- Assets/Systems/Multiplayer/Scripts/NetworkFungal.cs
+++ Assets/Systems/Multiplayer/Scripts/NetworkFungal.cs
@@ -8,6 +8,7 @@ public class NetworkFungal : NetworkBehaviour
     [SerializeField] private FungalInventory fungalInventory;
 
     private Health health;
+    private Movement movement;
 
     public NetworkVariable<float> Health = new NetworkVariable<float>(0f, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
     public event UnityAction OnHealthDepleted;
@@ -22,6 +23,7 @@ public class NetworkFungal : NetworkBehaviour
         fungalController.InitializeAnimations();
 
         health = GetComponent<Health>();
+        movement = GetComponent<Movement>();
 
         if (IsServer)
         {
@@ -40,6 +42,16 @@ public class NetworkFungal : NetworkBehaviour
     public void TakeDamageServerRpc(float damage)
     {
         Health.Value = Mathf.Clamp(Health.Value - damage, 0, health.MaxHealth);
-        if (Health.Value == 0) OnHealthDepleted?.Invoke();
+        if (Health.Value == 0)
+        {
+            OnHealthDepleted?.Invoke();
+            OnHealthDepletedClientRpc();
+        }
+    }
+
+    [ClientRpc]
+    private void OnHealthDepletedClientRpc()
+    {
+        movement.Stop();
     }
 }
