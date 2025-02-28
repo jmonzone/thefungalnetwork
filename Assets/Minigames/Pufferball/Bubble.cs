5ddbcefba8265f9c0ddba6abc46bbc172e897006
Jonan <jjsalomonzon@gmail.com>
2025-02-27 22:58:38 -0800

    added bubble attack

---
+++ Assets/Minigames/Pufferball/Bubble.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class Bubble : NetworkBehaviour
+{
+    //[SerializeField] private Gam
+    private void Update()
+    {
+        if (IsOwner)
+        {
+            CheckPlayerHit();
+        }
+    }
+
+    private void CheckPlayerHit()
+    {
+        // Detect all players in radius
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 1f);
+        foreach (Collider hit in hitColliders)
+        {
+            var fungal = hit.GetComponent<NetworkFungal>();
+            if (fungal != null)
+            {
+                fungal.TakeDamageServerRpc(1f);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
